f03fb3db52ec7980a5cab5afd6297f509cca7bd4
daiki <xuvy73c6eqkrabbiw@icloud.com>
2026-07-02 06:26:38 +0900

    メール認証フローの微修正

--- routes/web.php
+++ routes/web.php
@@ -11,38 +11,39 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+// ログイン不要エリア
 Route::get('/', [ItemController::class, 'index'])->name('items.index');
 Route::get('/item/{item_id}', [ItemController::class, 'show'])->name('items.show');
-
 Route::view('/login', 'auth.login')->name('login');
 Route::view('/register', 'auth.register')->name('register');
+
 Route::post('/login-action', function (Request $request) {
     if (Auth::attempt($request->only('email', 'password'))) {
         $request->session()->regenerate();
         return redirect()->route('items.index');
     }
     return back()->withErrors(['email' => 'ログイン情報が登録されていません']);
-    })->name('login.action');
+})->name('login.action');
 
-Route::middleware('auth')->group(function () {
-    
+Route::middleware(['auth'])->group(function () {
     Route::get('/email/verify', function () {
         return view('auth.verify-email');
     })->name('verification.notice');
 
     Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
         $request->fulfill();
-        return redirect()->route('mypage.profile'); 
+        return redirect()->route('mypage.profile');
     })->middleware(['signed'])->name('verification.verify');
 
     Route::post('/email/verification-notification', function (Request $request) {
         $request->user()->sendEmailVerificationNotification();
         return back()->with('message', '認証メールを再送信しました！');
     })->middleware(['throttle:6,1'])->name('verification.send');
+});
 
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/mypage/profile', [ProfileController::class, 'index'])->name('mypage.profile');
     Route::post('/mypage/profile', [ProfileController::class, 'update'])->name('mypage.update');
-
     Route::get('/mypage/show_list', [ProfileController::class, 'showList'])->name('mypage.show_list');
     
     Route::post('/item/{item}/comment', [CommentController::class, 'store'])->name('items.comment');
